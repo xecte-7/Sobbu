420a62791cbca7bb774a2477a679c41cd24255aa
XECTE-7 <43646493+xecte-7@users.noreply.github.com>
2023-04-21 13:26:51 +0800

    version 1.3

--- sobbu.py
+++ sobbu.py
@@ -1,6 +1,8 @@
 #!/usr/env python3
 # -*- encoding: utf-8 -*-
 
+import sourcedefender, os
+os.environ['SOURCEDEFENDER_PASSWORD'] = 'xecte-7'
 from assets import sobbu_main as Sobbu
 
 if __name__ == '__main__':
