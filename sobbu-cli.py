d27e41a9362228da94477875c763cfac7e2efd9b
XECTE-7 <43646493+xecte-7@users.noreply.github.com>
2023-04-21 17:00:12 +0800

    Delete sobbu-cli.py

--- sobbu-cli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/env python3
-# -*- encoding: utf-8 -*-
-
-import sourcedefender, os
-os.environ['SOURCEDEFENDER_PASSWORD'] = 'xecte-7'
-from assets import sobbu_main as Sobbu
-
-if __name__ == '__main__':
-	Sobbu.utama()
